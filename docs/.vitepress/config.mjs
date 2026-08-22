0e98ce401bd2f9c73738e02f7d358dc5eea88aff
SeerGlaucus <SeerGlaucus@users.noreply.github.com>
2026-08-22 22:26:35 +0800

    docs: add piercing line article

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -19,7 +19,8 @@ export default defineConfig({
             { text: '00 订单簿基础', link: '/candlestick/00-订单簿基础' },
             { text: '01 锤子线与上吊线', link: '/candlestick/01-锤子线与上吊线' },
             { text: '02 吞没形态', link: '/candlestick/02-吞没形态' },
-            { text: '03 乌云盖顶', link: '/candlestick/03-乌云盖顶' }
+            { text: '03 乌云盖顶', link: '/candlestick/03-乌云盖顶' },
+            { text: '04 刺透形态', link: '/candlestick/04-刺透形态' }
           ]
         }
       ]
